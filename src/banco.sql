48adaa75f9ff060d585303f740876bdea65eed0e
AlxSndBR2 <77alex77sandro77@gmail.com>
2026-05-20 17:09:53 +0000

    Atualizacao banco de dados

--- src/banco.sql
+++ src/banco.sql
@@ -1,7 +1,15 @@
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE transacoes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     valor DECIMAL(10,2) NOT NULL,
     tipo VARCHAR(50) NOT NULL,
     data_transacao DATE NOT NULL,
-    categoria VARCHAR(100) NOT NULL
+    categoria VARCHAR(100) NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
--- src/teste_banco.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'conecta.php';
-
-if ($pdo) {
-    echo "<h1>Conexão com o banco 'app_db' feita com sucesso! 🚀</h1>";
-} else {
-    echo "<h1>Erro na conexão.</h1>";
-}
-?>
